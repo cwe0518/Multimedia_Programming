a4663a944b576aa0f30ce325974e0ab976a26651
cwe0518 <137860700+cwe0518@users.noreply.github.com>
2024-03-13 13:11:00 +0900

    Add files via upload

---
+++ test.java
@@ -0,0 +1,18 @@
+package cha02;
+
+import java.util.Scanner;
+
+public class test {
+
+	public static void main(String[] args) {
+		double w, h, a;
+		
+		Scanner in = new Scanner(System.in);
+		System.out.print("직사각형의 가로 길이를 입력하세요 : \n");
+		w = in.nextDouble();
+		System.out.print("직사각형의 세로 길이를 입력하세요 : \n");
+		h = in.nextDouble();
+		a = w * h;
+		System.out.printf("직사각형의 넓이 : %f", a);
+	}
+}
