ac0a112c20eee6cd6f9cf3bab731e12cea860513
cwe0518 <137860700+cwe0518@users.noreply.github.com>
2024-03-14 14:02:04 +0900

    Add files via upload

---
+++ 실습/cha02/Num_01.java
@@ -0,0 +1,14 @@
+package page80;
+
+public class Num_01 {
+
+	public static void main(String[] args) {
+		System.out.println("     *     ");
+		System.out.println("    ***    ");
+		System.out.println("   *****   ");
+		System.out.println("  *******  ");
+		System.out.println(" ********* ");
+		System.out.println("***********");
+	}
+
+}
