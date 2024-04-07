318b64d6264a3a13536fc3d3cb8bab1dac214762
cwe0518 <137860700+cwe0518@users.noreply.github.com>
2024-04-07 11:54:06 +0900

    Add files via upload

---
+++ 실습/ArrayListDemo.java
@@ -0,0 +1,23 @@
+package cha05;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		ArrayList<Integer> scores = new ArrayList<>();
+		int data, sum = 0;
+		
+		while((data = in.nextInt()) >= 0)
+			scores.add(data);
+		
+		for(int i = 0; i < scores.size(); i++)
+			sum += scores.get(i);
+		
+		
+		System.out.println("ЦђБе = " + (double)sum / scores.size());
+	}
+
+}
